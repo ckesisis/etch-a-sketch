a4b4741d3c39872ce036bf091e46226e6e0d240e
Chris Kesisis <ckesisis@gmail.com>
2023-12-31 17:35:55 +0200

    Final Project on the Odin Project Foundation

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 window.addEventListener('load', () => {
-	const tools = document.querySelector('.tools-area');
+	document.querySelector('#selectSquares').addEventListener('click', () => {
+		let squares = prompt('Please enter squares');
 
-	for (let i = 0; i < 12; i++) {
-		const newDiv = document.createElement('div');
-		newDiv.classList.add('flex-container');
-		tools.appendChild(newDiv);
-		for (let j = 0; j < 12; j++) {
-			const newDiv2 = document.createElement('div');
-			newDiv2.classList.add('flex-item');
-			newDiv.appendChild(newDiv2);
+		const tools = document.querySelector('.tools-area');
+		tools.innerHTML = '';
+		const toolsWidth = tools.offsetWidth;
+
+		for (let i = 0; i < squares; i++) {
+			const newDiv = document.createElement('div');
+			newDiv.classList.add('flex-container');
+			tools.appendChild(newDiv);
+			for (let j = 0; j < squares; j++) {
+				const newDiv2 = document.createElement('div');
+				newDiv2.classList.add('flex-item');
+				newDiv2.style.width = toolsWidth / squares + 'px';
+				newDiv2.style.height = toolsWidth / squares + 'px';
+				newDiv.appendChild(newDiv2);
+			}
 		}
-	}
+
+		const flexItemsDivs = document.querySelectorAll('.flex-item');
+
+		flexItemsDivs.forEach((element) => {
+			element.addEventListener('mouseover', () => {
+				element.style.backgroundColor = 'red';
+			});
+		});
+	});
 });
